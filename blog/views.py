6a9e6ff725c53110ef1f122edd9743d49d3f44b0
Richard Lindsay <r.lnz100@gmail.com>
2016-10-08 14:54:44 +0000

    Add login; not working

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from flask import request, redirect, url_for
+from flask.ext.login import login_required
 
 from . import app
 from .database import session, Entry
@@ -34,10 +35,12 @@ def entries(page=1):
     )
     
 @app.route("/entry/add", methods=["GET"])
+@login_required
 def add_entry_get():
     return render_template("add_entry.html")
 
 @app.route("/entry/add", methods=["POST"])
+@login_required
 def add_entry_post():
     entry = Entry(
         title = request.form["title"],
@@ -78,6 +81,28 @@ def delete_entry(id):
         session.commit()
         return redirect(url_for("entries"))
     return render_template("delete_entry.html", entry = entry)
+    
+# Login View
+@app.route("/login", methods=["GET"])
+def login_get():
+    return render_template("login.html")
+    
+# Login POST
+from flask import flash
+from flask.ext.login import login_user
+from werkzeug.security import check_password_hash
+from .database import User
+
+@app.route("/login", methods=["POST"])
+def login_post():
+    email = request.form["email"]
+    password = request.form["password"]
+    user = session.query(User).filter_by(email=email).first()
+    if not user or not check_password_hash(user.password, password):
+        flash("Incorrect username or password", "danger")
+        return redirect(url_for("login_get"))
+    login_user(user)
+    return redirect(request.args.get('next')  or url_for("entries"))
     
         
     
